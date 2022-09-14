15443e602afd2b5117616141e9780d20720a6a85
“Chris <chrisanderson@developersinstitute.co.nz>
2022-09-15 11:03:35 +1200

    added fa icons to footer

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -1,5 +1,25 @@
+import {
+  FaReact,
+  FaNode,
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaGithub,
+  FaGit,
+} from "react-icons/fa";
+
 const Footer = () => {
-  return;
+  return (
+    <div className="text-3xl grid grid-cols-7">
+      <FaReact />
+      <FaNode />
+      <FaHtml5 />
+      <FaCss3 />
+      <FaJs />
+      <FaGithub />
+      <FaGit />
+    </div>
+  );
 };
 
 export default Footer;
