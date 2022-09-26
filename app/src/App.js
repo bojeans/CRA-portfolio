316e82e9ca8f1f1156eb5f1cb4e9a082c632e6b4
Chris Anderson <chris.d.anderson87@gmail.com>
2022-09-26 13:43:01 +1300

    added hamburger menu navbar, still to customise

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
+      <NavBar />
       <Header />
       <Footer />
     </>
