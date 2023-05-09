54c5d31bfc2d626646140a1f4ea14036e3674e66
Chris Anderson <chris.d.anderson87@gmail.com>
2023-05-09 15:49:23 +0800

    added in header, footer components to contact form

--- app/src/components/Contact.js
+++ app/src/components/Contact.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
 
 const Contact = () => {
   const [formState] = useState({});
@@ -35,120 +37,124 @@ const Contact = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
-        <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Contact
-          </h2>
-        </div>
-        <form onSubmit={handleSubmit} className="mt-8 space-y-6">
-          <input type="hidden" name="remember" defaultValue="true" />
-          <div className="rounded-md shadow-sm -space-y-px">
-            <div>
-              <label htmlFor="name" className="sr-only">
-                Name
-              </label>
-              <input
-                id="name"
-                name="name"
-                type="text"
-                autoComplete="name"
-                required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-                placeholder="Name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </div>
-            <div>
-              <label htmlFor="email" className="sr-only">
-                Email
-              </label>
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div>
-              <label htmlFor="location" className="sr-only">
-                Location
-              </label>
-              <select
-                id="location"
-                name="location"
-                autoComplete="location"
-                className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-                value={location}
-                onChange={(e) => setLocation(e.target.value)}
-              >
-                <option value="Placeholder">
-                  Optional - Click here to select your location
-                </option>
-                <option value="New Zealand">New Zealand</option>
-                <option value="Australia">Australia</option>
-                <option value="Other">Other</option>
-              </select>
-              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg
-                  className="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
+    <>
+      <NavBar />
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Contact
+            </h2>
+          </div>
+          <form onSubmit={handleSubmit} className="mt-8 space-y-6">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="name" className="sr-only">
+                  Name
+                </label>
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  autoComplete="name"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="email" className="sr-only">
+                  Email
+                </label>
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="location" className="sr-only">
+                  Location
+                </label>
+                <select
+                  id="location"
+                  name="location"
+                  autoComplete="location"
+                  className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                  value={location}
+                  onChange={(e) => setLocation(e.target.value)}
                 >
-                  <path d="M14.95 7.95a1 1 0 01-1.41 1.41l-2.54-2.53-2.54 2.53a1 1 0 01-1.41-1.41l2.54-2.53-2.54-2.54a1 1 0 011.41-1.41l2.54 2.54 2.54-2.54a1 1 0 011.41 1.41l-2.54 2.53 2.54 2.53z"></path>
-                </svg>
+                  <option value="Placeholder">
+                    Optional - Click here to select your location
+                  </option>
+                  <option value="New Zealand">New Zealand</option>
+                  <option value="Australia">Australia</option>
+                  <option value="Other">Other</option>
+                </select>
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                  <svg
+                    className="fill-current h-4 w-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M14.95 7.95a1 1 0 01-1.41 1.41l-2.54-2.53-2.54 2.53a1 1 0 01-1.41-1.41l2.54-2.53-2.54-2.54a1 1 0 011.41-1.41l2.54 2.54 2.54-2.54a1 1 0 011.41 1.41l-2.54 2.53 2.54 2.53z"></path>
+                  </svg>
+                </div>
+              </div>
+              <div>
+                <label htmlFor="description" className="sr-only">
+                  Description
+                </label>
+                <textarea
+                  id="description"
+                  name="description"
+                  autoComplete="description"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                  placeholder="Description"
+                  rows="5"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                ></textarea>
               </div>
             </div>
             <div>
-              <label htmlFor="description" className="sr-only">
-                Description
-              </label>
-              <textarea
-                id="description"
-                name="description"
-                autoComplete="description"
-                required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-                placeholder="Description"
-                rows="5"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-              ></textarea>
-            </div>
-          </div>
-          <div>
-            <button
-              type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            >
-              <span
-                className="absolute left-0 inset-y-0 flex items-center pl-3"
-                aria-hidden="true"
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
               >
-                <svg
-                  className="h-5 w-5 text-blue-500 group-hover:text-blue-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
+                <span
+                  className="absolute left-0 inset-y-0 flex items-center pl-3"
                   aria-hidden="true"
-                ></svg>
-              </span>
-              Submit
-            </button>
-          </div>
-        </form>
-        {formSubmitted && (
-          <p className="text-green-500">Thank you for submitting the form!</p>
-        )}
+                >
+                  <svg
+                    className="h-5 w-5 text-blue-500 group-hover:text-blue-400"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  ></svg>
+                </span>
+                Submit
+              </button>
+            </div>
+          </form>
+          {formSubmitted && (
+            <p className="text-green-500">Thank you for submitting the form!</p>
+          )}
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 export default Contact;
