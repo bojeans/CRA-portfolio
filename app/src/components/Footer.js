0c66bff4ae208aca9e7110e64ba596dd35db357f
Chris Anderson <chris.d.anderson87@gmail.com>
2023-05-23 15:38:00 +0800

    minor styling tweaks on footer

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -8,35 +8,30 @@ const linkedin = process.env.REACT_APP_LINKEDIN_ENDPOINT;
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800">
+    <footer>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="ml-3 text-white md:text-2xl text-1.5xl mt-10 mb-2">
+        <div className="ml-3 md:text-2xl text-1.5xl mt-10 mb-2">
           <p>Chris Anderson</p>
           <p>Web Developer 2020 - present date </p>
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between h-16">
-          <ul className="flex md:flex-row md:space-x-4">
-            <li>
-              <a
-                href={cv}
-                className="text-gray-50 hover:text-white"
-                target="_blank"
-                rel="noreferrer"
-              >
+          <ul className="flex flex-col md:flex-row md:space-x-4">
+            <li className="mb-3 hover:bg-gray-400 px-2 py-1 rounded-md">
+              <a href={cv} target="_blank" rel="noreferrer">
                 <FaRegUser />
               </a>
             </li>
-            <li className="mb-3 text-blue-800">
+            <li className="mb-3 hover:bg-gray-400 px-2 py-1 rounded-md text-blue-800">
               <a href={linkedin} target="_blank" rel="noreferrer">
                 <FaLinkedin />
               </a>
             </li>
-            <li className="mb-3">
+            <li className="mb-3 hover:bg-gray-400 px-2 py-1 rounded-md">
               <a href={github} target="_blank" rel="noreferrer">
                 <FaGithub />
               </a>
             </li>
-            <li className="mb-3 text-blue-800">
+            <li className="mb-3 hover:bg-gray-400 px-2 py-1 rounded-md text-blue-800">
               <a href={facebook} target="_blank" rel="noreferrer">
                 <FaFacebookF />
               </a>
