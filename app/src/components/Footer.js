e6c427f389c6f2a4fe24eb22c338f59bcc787cb6
Chris Anderson <chris.d.anderson87@gmail.com>
2023-05-23 15:14:40 +0800

    added social media into env variables

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -1,42 +1,50 @@
 import { FaGithub, FaFacebookF, FaLinkedin, FaRegUser } from "react-icons/fa";
 import cv from "../assets/cv.pdf";
 
-const github = "https://github.com/bojeans";
-const facebook = "https://www.facebook.com/chris.anderson.58152/";
-const linkedin = "https://www.linkedin.com/in/chris-anderson-4b289b189/";
+// social link global variables
+const github = process.env.REACT_APP_FACEBOOK_ENDPOINT;
+const facebook = process.env.REACT_APP_GITHUB_ENDPOINT;
+const linkedin = process.env.REACT_APP_LINKEDIN_ENDPOINT;
 
 const Footer = () => {
   return (
-    <>
-      <div className="ml-3 md:text-2xl text-1.5xl mt-10 mb-2">
-        <p>Chris Anderson</p>
-        <p>Web Developer 2020 - present date </p>
+    <footer className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="ml-3 text-white md:text-2xl text-1.5xl mt-10 mb-2">
+          <p>Chris Anderson</p>
+          <p>Web Developer 2020 - present date </p>
+        </div>
+        <div className="flex flex-col md:flex-row items-center justify-between h-16">
+          <ul className="flex md:flex-row md:space-x-4">
+            <li>
+              <a
+                href={cv}
+                className="text-gray-50 hover:text-white"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaRegUser />
+              </a>
+            </li>
+            <li className="mb-3 text-blue-800">
+              <a href={linkedin} target="_blank" rel="noreferrer">
+                <FaLinkedin />
+              </a>
+            </li>
+            <li className="mb-3">
+              <a href={github} target="_blank" rel="noreferrer">
+                <FaGithub />
+              </a>
+            </li>
+            <li className="mb-3 text-blue-800">
+              <a href={facebook} target="_blank" rel="noreferrer">
+                <FaFacebookF />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="flex items-center justify-center font-bold ml-3">
-        <ul>
-          <li className="mb-3">
-            <a href={cv} target="_blank" rel="noreferrer">
-              <FaRegUser />
-            </a>
-          </li>
-          <li className="mb-3 text-blue-800">
-            <a href={linkedin} target="_blank" rel="noreferrer">
-              <FaLinkedin />
-            </a>
-          </li>
-          <li className="mb-3">
-            <a href={github} target="_blank" rel="noreferrer">
-              <FaGithub />
-            </a>
-          </li>
-          <li className="mb-3 text-blue-800">
-            <a href={facebook} target="_blank" rel="noreferrer">
-              <FaFacebookF />
-            </a>
-          </li>
-        </ul>
-      </div>
-    </>
+    </footer>
   );
 };
 
