4543f07b8241a13ec83e464c704e22598c194e14
Chris Anderson <chris.d.anderson87@gmail.com>
2023-05-23 12:00:29 +0800

    adding in icons

--- app/src/components/About.js
+++ app/src/components/About.js
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
 import cv from "../assets/cv.pdf";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import { FaGithub, FaFacebookF, FaLinkedin, FaRegUser } from "react-icons/fa";
 
 const About = () => {
   return (
@@ -10,10 +12,41 @@ const About = () => {
         About Me
       </h1>
 
-      <div className="text-1.5xl text-blue-800 font-bold ml-3 md:text-2xl">
-        <a href={cv} target="_blank" rel="noreferrer">
-          1 Page CV/Resume
-        </a>
+      <div className="flex text-3xl items-center justify-center font-bold ml-3 md:text-2xl">
+        <ul>
+          <li className="mb-3">
+            <Link to={cv} target="_blank" rel="noreferrer">
+              <FaRegUser />
+            </Link>
+          </li>
+          <li className="mb-3 text-blue-800">
+            <Link
+              to="https://www.linkedin.com/in/chris-anderson-4b289b189/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaLinkedin />
+            </Link>
+          </li>
+          <li className="mb-3">
+            <Link
+              to={"https://github.com/bojeans"}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaGithub />
+            </Link>
+          </li>
+          <li className="mb-3 text-blue-800">
+            <Link
+              to={"https://www.facebook.com/chris.anderson.58152/"}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaFacebookF />
+            </Link>
+          </li>
+        </ul>
       </div>
       <Footer />
     </div>
