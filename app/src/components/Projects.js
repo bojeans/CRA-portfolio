4b338fc8522ba9da00699dd43ae76c703aefe927
Chris Anderson <chris.d.anderson87@gmail.com>
2023-11-07 16:59:05 +1300

    updating project page status

--- app/src/components/Projects.js
+++ app/src/components/Projects.js
@@ -17,7 +17,7 @@ const Projects = () => {
                   Planning pipeline
                 </span>
               </h2>
-              <p>Budgetting App - no repo yet.</p>
+
               <p>
                 <a
                   href="https://github.com/bojeans/PERN-travel-blog"
@@ -29,22 +29,6 @@ const Projects = () => {
                 </a>
                 - React with Vite, supabase
               </p>
-              <p className="mt-3">
-                Planning to get across some more using chatGPT tutorials. Would
-                also like to checkout Python a bit more and developments on the
-                AI/machine learning space - if anyone has any
-                beginner/intermediate project ideas here or wants to
-                collaborate. Have been following along a little to kaggle.com
-                content
-              </p>
-            </div>
-          </div>
-          <div className="w-full md:w-1/3 px-4">
-            <div className="bg-gray-100 rounded-lg shadow-md p-4 mb-4 h-full flex flex-col">
-              <h2 className="text-lg font-bold mb-2">
-                <span className="border-b border-gray-500">In Progress</span>
-              </h2>
-
               <p>
                 <a
                   href="https://github.com/bojeans/rps"
@@ -55,6 +39,24 @@ const Projects = () => {
                   <strong>Rock, Paper, Scissors</strong>
                 </a>
                 - javascript DOM refresher, basic CSS3 styling.
+              </p>
+            </div>
+          </div>
+          <div className="w-full md:w-1/3 px-4">
+            <div className="bg-gray-100 rounded-lg shadow-md p-4 mb-4 h-full flex flex-col">
+              <h2 className="text-lg font-bold mb-2">
+                <span className="border-b border-gray-500">In Progress</span>
+              </h2>
+              <p>
+                <a
+                  href="https://github.com/bojeans/budgetting-app"
+                  className="text-blue-800"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <strong>Budgetting App</strong>{" "}
+                </a>
+                - Fullstack Application
               </p>
             </div>
           </div>
