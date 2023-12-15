e3d1c14596ba050ad627e0e48da099c0f247623a
Chris Anderson <chrisanderson@developers.institute.co.nz>
2023-12-15 13:33:17 +1300

    broke up image into separate component

--- app/src/components/LandingPage.js
+++ app/src/components/LandingPage.js
@@ -1,4 +1,4 @@
-import pic from "../assets/me.png";
+import Image from "./Image";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
 import Contact from "./Contact";
@@ -8,11 +8,7 @@ const LandingPage = () => {
     <>
       <NavBar />
       <div className="flex flex-col items-center space-y-12">
-        <img
-          src={pic}
-          alt="headshot of author"
-          className="max-w-xl mx-auto mt-12 mb-5 w-40 md:w-80 h-auto md:h-50"
-        ></img>
+        <Image />
         <Contact />
       </div>
       <Footer />
