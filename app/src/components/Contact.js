72197569bef2ada99b7dd01b845f37cfae73febb
Chris Anderson <chrisanderson@developers.institute.co.nz>
2023-12-15 15:02:44 +1300

    updates on contact form, tested, added submit limitations

--- app/src/components/Contact.js
+++ app/src/components/Contact.js
@@ -1,136 +1,103 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useForm, ValidationError } from "@formspree/react";
 
 const Contact = () => {
-  const [submitted, setSubmitted] = useState(false);
-  const [submitting, setSubmitting] = useState(false);
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    description: "",
-  });
+  const [state, handleSubmit, reset] = useForm("xnqwbjwd");
+  const [submitCount, setSubmitCount] = useState(0);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
-  const onChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
-  };
+  const formRef = useRef();
 
-  const url = process.env.REACT_APP_FORMSPREE_ENDPOINT;
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
-      if (response.ok) {
-        setSubmitting(true);
-        setSubmitted(true);
-        setFormData({ name: "", email: "", description: "" });
-        alert("Form submitted successfully!");
-        setSubmitting(false);
-      } else {
-        throw new Error("Form submission failed");
+  useEffect(() => {
+    if (state.succeeded) {
+      setSubmitCount((prevCount) => prevCount + 1);
+      setShowSuccessMessage(true);
+      if (formRef.current) {
+        formRef.current.reset();
       }
-    } catch (error) {
-      console.log(error);
+      reset();
     }
+  }, [state.succeeded, reset]);
+
+  const handleClick = (e) => {
+    e.target.select();
   };
 
   return (
-    <>
-      <div className="flex flex-col items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-md w-full space-y-8">
-          <div>
-            <h2 className="text-center text-2xl font-bold text-gray-900">
-              I'm potentially open contract work and/or collaborating on a
-              coding project, feel free to drop me an email using the contact
-              form below.
-            </h2>
+    <div className="flex flex-col items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+      <span className="max-w-md w-full space-y-8">
+        <h2 className="text-center text-2xl font-bold text-gray-900">
+          If you want to get in touch regarding potential contract work and/or
+          collaborating on a coding project, feel free to drop me an email using
+          the contact form below.
+        </h2>
+      </span>
+      <form ref={formRef} onSubmit={handleSubmit} className="w-full max-w-lg">
+        {submitCount >= 2 && (
+          <p className="text-red-600 font-bold">
+            You have exceeded the maximum number of submits. Please try again
+            later.
+          </p>
+        )}
+        {showSuccessMessage && submitCount < 2 && (
+          <p className="text-green-600 font-bold">
+            Thanks for contacting me. Your message has successfully submitted
+          </p>
+        )}
+
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="email"
+            >
+              Email Address
+            </label>
+            <input
+              onDoubleClick={handleClick}
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              id="email"
+              type="email"
+              name="email"
+              required
+            />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
           </div>
-          <form
-            onSubmit={handleSubmit}
-            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-          >
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 font-bold mb-2"
-                htmlFor="name"
-              >
-                Name
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="name"
-                name="name"
-                type="text"
-                placeholder="Enter your name"
-                value={formData.name}
-                onChange={onChange}
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 font-bold mb-2"
-                htmlFor="email"
-              >
-                Email
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="email"
-                name="email"
-                type="email"
-                placeholder="Enter your email"
-                value={formData.email}
-                onChange={onChange}
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 font-bold mb-2"
-                htmlFor="description"
-              >
-                Description
-              </label>
-              <textarea
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="description"
-                name="description"
-                placeholder="Enter your message"
-                value={formData.description}
-                onChange={onChange}
-                required
-              />
-            </div>
-            <div className="flex items-center justify-between">
-              <button
-                className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="submit"
-              >
-                Submit
-              </button>
-              {submitting && (
-                <span className="text-gray-700 italic text-sm">
-                  Submitting...
-                </span>
-              )}
-              {submitted && (
-                <span className="text-green-600 italic text-sm">
-                  Thank you for your submission!
-                </span>
-              )}
-            </div>
-          </form>
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="message"
+            >
+              Message
+            </label>
+            <textarea
+              onDoubleClick={handleClick}
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="message"
+              name="message"
+              required
+            />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
+          </div>
         </div>
-      </div>
-    </>
+        <button
+          type="submit"
+          disabled={submitCount >= 2}
+          className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
   );
 };
+
 export default Contact;
