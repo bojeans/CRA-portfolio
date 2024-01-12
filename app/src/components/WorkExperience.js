4ae446ccfa452ed22a93be23555b99ed0591de57
Chris Anderson <chrisanderson@developers.institute.co.nz>
2024-01-12 17:38:10 +1300

    updated work experience links

--- app/src/components/WorkExperience.js
+++ app/src/components/WorkExperience.js
@@ -3,17 +3,24 @@ import NavBar from "./NavBar";
 import Footer from "./Footer";
 import WorkExperienceCard from "./WorkExperienceCard";
 
-const website = "https://www.facebook.com/developersnz/";
-const website2 = "https://www.youtube.com/watch?v=bq9TF6BFJHo";
-
 const fbLink = (
-  <a href={website} target="_blank" rel="noopener noreferrer">
+  <a
+    href="https://www.facebook.com/developersnz/"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="text-blue-600"
+  >
     DI facebook
   </a>
 );
 
 const ytLink = (
-  <a href={website2} target="_blank" rel="noopener noreferrer">
+  <a
+    href="https://www.youtube.com/watch?v=bq9TF6BFJHo"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="text-blue-600"
+  >
     Hack-a-thon
   </a>
 );
@@ -40,7 +47,25 @@ const WorkExperience = () => {
             company="Developers Institute NZ Ltd"
             position="Associate Software Developer & Remote Teaching Assistant"
             date="April 2021 - August 2023"
-            description={`${fbLink} Worked in a platform team on a SaaS project. Involved integrating our organisation onto Okta and syncing up our various apps. A lot of this was learning as we went, so documented everything in confluence and worked in an agile environment with weekly sprints. We also had a help desk type ticketing automation system so students could lodge a support request ticket to us. So I also had a lot of direct contact with students (customers) on a regular basis. Matters such as okta login problems, exercise/project assistance helping them debug code, mental health/anxiety reasons. Quite a challenging but fun role given I am reasonably new to the industry as well. Was also fortunate enough to represent Developers Institute externally at a 1 week hackathon in Whangarei with a client Northpower and AWS reps. Short, somewhat uninformative, video they made of that experience if you’re interested: ${ytLink}`}
+            description={
+              <>
+                {fbLink} Worked in a platform team on a SaaS project. Involved
+                integrating our organisation onto Okta and syncing up our
+                various apps. A lot of this was learning as we went, so
+                documented everything in confluence and worked in an agile
+                environment with weekly sprints. We also had a help desk type
+                ticketing automation system so students could lodge a support
+                request ticket to us. So I also had a lot of direct contact with
+                students (customers) on a regular basis. Matters such as okta
+                login problems, exercise/project assistance helping them debug
+                code, mental health/anxiety reasons. Quite a challenging but fun
+                role given I am reasonably new to the industry as well. Was also
+                fortunate enough to represent Developers Institute externally at
+                a 1 week hackathon in Whangarei with a client Northpower and AWS
+                reps. Short, somewhat uninformative, video they made of that
+                experience if you’re interested: {ytLink}
+              </>
+            }
           />
         </div>
         <h2 className="text-3xl font-bold mt-8 border-b-2 border-gray-800 pb-2">
--- app/src/components/WorkExperienceCard.js
+++ app/src/components/WorkExperienceCard.js
@@ -1,90 +1,16 @@
 import React from "react";
-import NavBar from "./NavBar";
-import Footer from "./Footer";
-import WorkExperienceCard from "./WorkExperienceCard";
 
-const website = "https://www.facebook.com/developersnz/";
-const website2 = "https://www.youtube.com/watch?v=bq9TF6BFJHo";
-
-const fbLink = (
-  <a href={website} target="_blank" rel="noopener noreferrer">
-    DI facebook
-  </a>
-);
-
-const ytLink = (
-  <a href={website2} target="_blank" rel="noopener noreferrer">
-    Hack-a-thon
-  </a>
-);
-
-const WorkExperience = () => {
+const WorkExperienceCard = ({ company, position, date, description }) => {
   return (
-    <>
-      <NavBar />
-      <div className="container mx-auto p-4">
-        <h2 className="text-3xl font-bold mb-4 border-b-2 border-gray-800 pb-2">
-          Tech Related
-        </h2>
-        <div className="flex flex-wrap justify-center">
-          <WorkExperienceCard
-            company="Rising Star Digital Ltd"
-            position="Contract Developer"
-            date="Nov 2023 - Present"
-            description="I worked on the frontend of the /trade page and /snipe page. Backend was pre-coded in C# by another dev, my role was essentially using Swagger for API specs to manage React state between several parent & child functional components. A smart search bar, trade history, top traded tokens, buy/sell widget were some of the features I worked on."
-          />
-          <WorkExperienceCard
-            company="Developers Institute NZ Ltd"
-            position="Associate Software Developer & Remote Teaching Assistant"
-            date="April 2021 - August 2023"
-            description={
-              <>
-                {fbLink} Worked in a platform team on a SaaS project. Involved
-                integrating our organisation onto Okta and syncing up our
-                various apps. A lot of this was learning as we went, so
-                documented everything in confluence and worked in an agile
-                environment with weekly sprints. We also had a help desk type
-                ticketing automation system so students could lodge a support
-                request ticket to us. So I also had a lot of direct contact with
-                students (customers) on a regular basis. Matters such as okta
-                login problems, exercise/project assistance helping them debug
-                code, mental health/anxiety reasons. Quite a challenging but fun
-                role given I am reasonably new to the industry as well. Was also
-                fortunate enough to represent Developers Institute externally at
-                a 1 week hackathon in Whangarei with a client Northpower and AWS
-                reps. Short, somewhat uninformative, video they made of that
-                experience if you’re interested: {ytLink}
-              </>
-            }
-          />
-        </div>
-        <h2 className="text-3xl font-bold mt-8 border-b-2 border-gray-800 pb-2">
-          Non-Tech Related - Coming Soon
-        </h2>
-        <div className="flex flex-wrap justify-center">
-          <WorkExperienceCard
-            company="Placeholder"
-            position="Placeholder"
-            date="Placeholder"
-            description="Placeholder"
-          />
-          <WorkExperienceCard
-            company="Placeholder"
-            position="Placeholder"
-            date="Placeholder"
-            description="Placeholder"
-          />
-          <WorkExperienceCard
-            company="Placeholder"
-            position="Placeholder"
-            date="Placeholder"
-            description="Placeholder"
-          />
-        </div>
+    <div className="bg-white rounded-lg overflow-hidden shadow-lg m-4 sm:w-1/2 lg:w-1/3 ">
+      <div className="px-6 py-4">
+        <h5 className="font-bold text-xl mb-2">{company}</h5>
+        <h6 className="text-gray-600 text-lg mb-2">{position}</h6>
+        <h6 className="text-gray-600 text-lg mb-2">{date}</h6>
+        <p className="text-gray-700 text-base">{description}</p>
       </div>
-      <Footer />
-    </>
+    </div>
   );
 };
 
-export default WorkExperience;
+export default WorkExperienceCard;
