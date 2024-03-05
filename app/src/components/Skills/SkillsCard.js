84d9c1b095c0a094639b9fc9c5a18b20b34440b2
Chris Anderson <chris.d.anderson87@gmail.com>
2024-03-06 11:12:45 +1300

    fixed icon with flex wrap

--- app/src/components/Skills/SkillsCard.js
+++ app/src/components/Skills/SkillsCard.js
@@ -5,7 +5,7 @@ const SkillsCard = ({ icon, experience, description }) => {
   return (
     <div className="bg-white rounded-lg overflow-hidden shadow-lg m-4 skills-page">
       <div className="px-6 py-4 dark-border">
-        <ul className="flex font-bold text-gray-400 text-3xl mt-2 mb-5 skills-icons">
+        <ul className="flex flex-wrap items-center justify-center font-bold text-gray-400 gap-6 text-3xl mt-2 mb-5 skills-icons overflow-x-auto">
           {icon}
         </ul>
         <h3 className="text-lg font-serif mb-2 work_experience_details">
