06ca631ec13e4ca8ac0b54c4c25cd6da5ea477df
Chris Anderson <chris.d.anderson87@gmail.com>
2024-03-05 16:49:50 +1300

    updating darkmode css on skills page

--- app/src/components/Skills/SkillsCard.js
+++ app/src/components/Skills/SkillsCard.js
@@ -3,9 +3,9 @@ import CollapsibleText from "../utils/CollapsibleText";
 
 const SkillsCard = ({ icon, experience, description }) => {
   return (
-    <div className="bg-white rounded-lg overflow-hidden shadow-lg m-4 sm:w-1/2 lg:w-1/3 workexperience-page">
+    <div className="bg-white rounded-lg overflow-hidden shadow-lg m-4 sm:w-1/2 lg:w-1/3 skills-page">
       <div className="px-6 py-4">
-        <ul className="flex font-bold text-3xl mt-2 mb-5 work_experience_company">
+        <ul className="flex font-bold text-3xl mt-2 mb-5 skills-icons">
           {icon}
         </ul>
         <h3 className="text-lg font-serif mb-2 work_experience_details">
