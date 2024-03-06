a33e9cd2b8d731e568f3fe946eed953a4d59f92f
Chris Anderson <chris.d.anderson87@gmail.com>
2024-03-06 16:06:00 +1300

    work exp styling updated

--- app/src/components/WorkExperience/WorkExperienceCard.js
+++ app/src/components/WorkExperience/WorkExperienceCard.js
@@ -12,7 +12,7 @@ const WorkExperienceCard = ({
 }) => {
   return (
     <div className="bg-white rounded-lg overflow-hidden shadow-lg m-4 sm:w-1/2 lg:w-1/3 workexperience-page">
-      <div className="px-6 py-4 dark-border">
+      <div className="px-6 py-4 w-full h-full dark-border">
         <h3 className="font-bold text-xl mb-2 work_experience_company">
           {company}
         </h3>
