380f48e45f75cc63d77f2eae073c80ec8ada63a8
Chris Anderson <chris.d.anderson87@gmail.com>
2024-06-22 15:13:17 +1200

    save point prior to tinkering with image heights

--- app/src/components/Qualifications/Qualifications.js
+++ app/src/components/Qualifications/Qualifications.js
@@ -2,6 +2,7 @@ import NavBar from "../NavBar";
 import Footer from "../Footer";
 import QualificationsCard from "./QualificationsCard";
 import Uni from "../../assets/waikatouni.jpg";
+import School from "../../assets/ncealvl3.jpg";
 
 const Qualifications = () => {
   return (
@@ -10,14 +11,32 @@ const Qualifications = () => {
       <h1 className="text-3xl font-bold projects-page text-center mt-6 mb-4 md:text-6xl">
         Qualifications
       </h1>
-      <div className="container mx-auto projects-page">
+      <div className="container mx-auto">
         <div className="flex flex-wrap justify-center">
           <QualificationsCard
-            image={Uni}
-            alt="University of Waikato qualification certificate"
             title="University of Waikato"
-            year="2006-2010"
+            year="2010"
             description="Bachelor of Management Studies. Majors in Information Systems & Strategic Management"
+            image={
+              <img
+                className="transform -rotate-90 w-full h-auto max-h-64"
+                src={Uni}
+                alt="University of Waikato qualification certificate"
+              />
+            }
+          />
+
+          <QualificationsCard
+            title="Tauraroa Area School"
+            year="2005"
+            description="NCEA Level 3"
+            image={
+              <img
+                className="transform -rotate-180 w-full h-auto max-h-64"
+                src={School}
+                alt="Tauraroa Area School qualification certificate"
+              />
+            }
           />
         </div>
         <Footer />
--- app/src/components/Qualifications/QualificationsCard.js
+++ app/src/components/Qualifications/QualificationsCard.js
@@ -1,23 +1,14 @@
 import React from "react";
 import CollapsibleText from "../utils/CollapsibleText";
 
-const QualificationsCard = ({ image, title, year, alt, description }) => {
+const QualificationsCard = ({ image, title, year, description }) => {
   return (
-    <div className="bg-white rounded-lg overflow-hidden shadow-lg m-4 projects-page md:gap-y-6">
-      <div className="h-full px-6 py-4 dark-border">
-        <h2 className="text-lg text-center font-sans mb-2 md:text-3xl lg:text-4xl">
-          {title}
-        </h2>
-        <h3 className="text-lg text-center font-sans mb-2 md:text-2xl lg:text-3xl">
-          {year}
-        </h3>
-        <div className="proj-Images font-bold text-gray-400 gap-2 text-3xl mb-2 overflow-x-auto ">
-          <img className="object-cover h-auto" src={image} alt={alt} />
-        </div>
-
-        <p className="font-serif text-base mt-5">
-          {description && <CollapsibleText text={description} />}
-        </p>
+    <div className="px-6 py-4 max-w-sm rounded overflow-hidden shadow-lg m-4">
+      {image}
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{title}</div>
+        <p className="text-base">{year}</p>
+        {description && <CollapsibleText text={description} />}
       </div>
     </div>
   );
