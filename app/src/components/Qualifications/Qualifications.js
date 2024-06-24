a262efb7d744f8304bd45c4fcf189cfbb430ca48
Chris Anderson <chris.d.anderson87@gmail.com>
2024-06-25 11:55:38 +1200

    adjusting quals light mode sections

--- app/src/components/Qualifications/Qualifications.js
+++ app/src/components/Qualifications/Qualifications.js
@@ -6,8 +6,8 @@ import Uni from "../../assets/waikatouni.jpg";
 import School from "../../assets/ncealvl3.jpg";
 import PP from "../../assets/peoplepotential.jpg";
 import ZoomableImage from "../utils/ZoomableImage";
-import ContainerBackground from "../utils/ContainerBackground";
 import "react-medium-image-zoom/dist/styles.css";
+import "../../index.css";
 
 const Qualifications = () => {
   return (
@@ -17,59 +17,61 @@ const Qualifications = () => {
       <h1 className="text-3xl font-bold projects-page text-center mt-6 mb-4 md:text-6xl">
         Qualifications
       </h1>
-      <ContainerBackground>
-        <div className="flex flex-wrap justify-center">
-          <QualificationsCard
-            title="People Potential"
-            year="2022"
-            description="Certificate in Adult Education & Training"
-            image={
-              <ZoomableImage
-                src={PP}
-                alt="People Potential qualification certificate"
-              />
-            }
-          />
-          <QualificationsCard
-            title="Developers Institute"
-            year="2021"
-            description="NZ diploma in Web Development & Design level 5"
-            image={
-              <ZoomableImage
-                src={DI}
-                alt="Developers Institute qualification certificate"
-              />
-            }
-          />
-          <QualificationsCard
-            title="University of Waikato"
-            year="2010"
-            description="Bachelor of Management Studies. Majors in Information Systems & Strategic Management"
-            image={
-              <ZoomableImage
-                className="w-full h-48 mt-6 mb-8"
-                src={Uni}
-                alt="University of Waikato qualification certificate"
-                rotation={-90}
-              />
-            }
-          />
-          <QualificationsCard
-            title="Tauraroa Area School"
-            year="2005"
-            description="NCEA Level 3"
-            image={
-              <ZoomableImage
-                src={School}
-                alt="Tauraroa Area School qualification certificate"
-                rotation={180}
-              />
-            }
-          />
-        </div>
 
-        <Footer />
-      </ContainerBackground>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+        <QualificationsCard
+          title="People Potential"
+          year="2022"
+          description="Certificate in Adult Education & Training"
+          image={
+            <ZoomableImage
+              className="qualification-image"
+              src={PP}
+              alt="People Potential qualification certificate"
+            />
+          }
+        />
+        <QualificationsCard
+          title="Developers Institute"
+          year="2021"
+          description="NZ diploma in Web Development & Design level 5"
+          image={
+            <ZoomableImage
+              className="qualification-image"
+              src={DI}
+              alt="Developers Institute qualification certificate"
+            />
+          }
+        />
+        <QualificationsCard
+          title="University of Waikato"
+          year="2010"
+          description="Bachelor of Management Studies. Majors in Information Systems & Strategic Management"
+          image={
+            <ZoomableImage
+              className="qualification-image"
+              src={Uni}
+              alt="University of Waikato qualification certificate"
+              rotation={-90}
+            />
+          }
+        />
+        <QualificationsCard
+          title="Tauraroa Area School"
+          year="2005"
+          description="NCEA Level 3"
+          image={
+            <ZoomableImage
+              className="qualification-image"
+              src={School}
+              alt="Tauraroa Area School qualification certificate"
+              rotation={180}
+            />
+          }
+        />
+      </div>
+
+      <Footer />
     </>
   );
 };
